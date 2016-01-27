84ae27e2f66695ed8b5d6cc7d5d8f10d0af92269
Lucas Fayolle <lfayolle1@gmail.com>
2016-01-27 15:09:48 +0100

    page contact link gmap

--- app/site/templates/contact.php
+++ app/site/templates/contact.php
@@ -8,7 +8,7 @@
         <h3><?php echo $page->title() ?></h3>
         <?php echo $page->text()->kirbytext() ?>
         <div class="contact-info">
-          <?php echo $page->contactInfos()->kirbytext() ?>
+          <a href="https://goo.gl/maps/9AU5yn6LYcJ2" target="_blank"><?php echo $page->contactInfos()->kirbytext() ?></a>
           <p class="phone">Phone: <a href="phone:7608646411">760.864.6411</a>   /   Fax: <a href="phone:7608644147">760.864.4147</a></p>
         </div>
         <a href="mailto:info@korakia.com" class="btn golden cta"><span>Email us</span></a>
